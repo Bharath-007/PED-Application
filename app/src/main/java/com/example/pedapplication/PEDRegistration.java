34cdeed01a5bf77270f26b6c95241a559b453b2a
Sathya Prakaash <sathyaprakaash112@gmail.com>
2021-11-12 15:30:05 +0530

    Initial commit 2

---
+++ app/src/main/java/com/example/pedapplication/PEDRegistration.java
@@ -0,0 +1,14 @@
+package com.example.pedapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PEDRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedregistration);
+    }
+}
